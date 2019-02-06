f556becb7f3ee4fedd5db86e010f1b8e5b0c2ff9
Junya Otsuki <otsuki@cmpt.phys.tohoku.ac.jp>
2019-02-06 09:59:52 +0900

    Add null solver

--- python/impurity_solvers/__init__.py
+++ python/impurity_solvers/__init__.py
@@ -1,9 +1,11 @@
 from .triqs_cthyb import TRIQSCTHYBSolver
 from .triqs_hubbard_I import TRIQSHubbardISolver
 from .alps_cthyb import ALPSCTHYBSolver
+from .null_solver import NullSolver
 
 solver_classes = {
     'TRIQS/cthyb' : TRIQSCTHYBSolver,
     'TRIQS/hubbard-I' : TRIQSHubbardISolver,
     'ALPS/cthyb': ALPSCTHYBSolver,
+    'null': NullSolver,
 }
