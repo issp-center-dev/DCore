ab9dae914afb5d2ac1d1a9f4775f0351960f1160
Nayuta Takemori <takemori@okayama-u.ac.jp>
2019-02-18 10:36:38 +0900

    add alps_ctseg

--- python/impurity_solvers/__init__.py
+++ python/impurity_solvers/__init__.py
@@ -2,10 +2,12 @@ from .triqs_cthyb import TRIQSCTHYBSolver
 from .triqs_hubbard_I import TRIQSHubbardISolver
 from .alps_cthyb import ALPSCTHYBSolver
 from .null_solver import NullSolver
+from .alps_ctseg import ALPSCTSEGSolver
 
 solver_classes = {
     'TRIQS/cthyb' : TRIQSCTHYBSolver,
     'TRIQS/hubbard-I' : TRIQSHubbardISolver,
     'ALPS/cthyb': ALPSCTHYBSolver,
     'null': NullSolver,
+	'ALPS/ctseg': ALPSCTSEGSolver,
 }
