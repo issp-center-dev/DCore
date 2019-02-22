838d9789e8aee113b4bedab602b3bba720e8f5bf
Junya Otsuki <j.otsuki.tohoku@gmail.com>
2019-02-22 18:58:32 +0900

    Improve dcore_check (#13)
    Improvements of dcore_check

--- python/dcore_check.py
+++ python/dcore_check.py
@@ -24,193 +24,280 @@ from pytriqs.gf.local import *
 
 from dmft_core import DMFTCoreSolver
 from matplotlib.gridspec import GridSpec
+import numpy
 
 from program_options import *
 
 
-def dcore_check(filename, fileplot=None):
-    """
-    Main routine for checking convergence
+class DMFTCoreCheck(object):
 
-    Parameters
-    ----------
-    filename : string
-        Input-file name
+    def __init__(self, ini_file):
+        """
+        Parameters
+        ----------
+        ini_file : string
+            Input-file name
+        """
 
-    fileplot : string
-        Output file name. File format is determined by the extension (pdf, eps, jpg, etc).
-    """
-    print("\n  @ Reading {0} ...".format(filename))
-    #
-    # Construct a parser with default values
-    #
-    pars = create_parser()
-    #
-    # Parse keywords and store
-    #
-    pars.read(filename)
-    p = pars.as_dict()
-    #
-    #
+        if os.path.isfile(ini_file) is False:
+            raise Exception("Input file '%s' does not exist." % ini_file)
 
-    # Just for convenience
-    #output_file = p["model"]["seedname"]+'.out.h5'
-    #output_group = 'dmft_out'
-    beta = p["system"]["beta"]
-    omega_check = p['tool']['omega_check']
-    gs = GridSpec(2, 1)
+        print("\n  @ Reading {0} ...".format(ini_file))
+        #
+        # Construct a parser with default values
+        #
+        pars = create_parser()
+        #
+        # Parse keywords and store
+        #
+        pars.read(ini_file)
+        self.p = pars.as_dict()
 
-    if fileplot is not None:  # if graph is to be printed in a file
+        # Just for convenience
+        #output_file = p["model"]["seedname"]+'.out.h5'
+        #output_group = 'dmft_out'
+        self.beta = self.p["system"]["beta"]
+        self.omega_check = self.p['tool']['omega_check']
+
+        #
+        # Load DMFT data
+        #
+        self.p['control']['restart'] = True
+        self.solver = DMFTCoreSolver(self.p["model"]["seedname"], self.p, read_only=True)
+        self.n_iter = self.solver.iteration_number
+        self.n_sh = self.solver.n_inequiv_shells
+        self.spin_names = self.solver.spin_block_names
+        self.shell_info = [self.solver.inequiv_shell_info(ish) for ish in range(self.n_sh)]
+        self.n_iw = self.p["system"]["n_iw"]
+
+        print("  Total number of Iteration: {0}".format(self.n_iter))
+
+        # if __plot_init() is called
+        self.plot_called = False
+
+
+    def print_chemical_potential(self):
+        """
+        print chemical potential
+        """
+
+        print("\n  Iter  Chemical-potential")
+        for itr in range(1, self.n_iter+1):
+            print("  {0} {1}".format(itr, self.solver.chemical_potential(itr)))
+
+
+    def __plot_init(self):
+        if self.plot_called:
+            self.plt.clf()
+            self.plt.figure(figsize=(8, 6))  # default
+            return
+        self.plot_called = True
+
         import matplotlib
         matplotlib.use('Agg')  # do not plot on x11
-    from pytriqs.plot.mpl_interface import oplot, plt
-    plt.figure(figsize=(8, 10))
+
+        from pytriqs.plot.mpl_interface import oplot, plt
+
+        self.plt = plt
+        self.oplot = oplot
 
 
-    #
-    # Load DMFT data
-    #
-    p['control']['restart'] = True
-    solver = DMFTCoreSolver(p["model"]["seedname"], p, read_only=True)
-    iteration_number = solver.iteration_number
-    nsh = solver.n_inequiv_shells
-    spn = solver.spin_block_names
-    shell_info = [solver.inequiv_shell_info(ish) for ish in range(nsh)]
+    def plot_sigma_ave(self, basename, fig_ext):
+        """
+        plot Sigma(iw) averaged over shell, spin and orbital for last several iterations
+        """
+        self.__plot_init()
 
-    #
-    # Chemical potential
-    #
-    print("  Total number of Iteration: {0}".format(iteration_number))
-    print("\n  Iter  Chemical-potential")
-    for itr in range(1, iteration_number+1):
-        print("  {0} {1}".format(itr, solver.chemical_potential(itr)))
+        sigma_ave = []
+        nsigma = 0
+        num_itr_plot = 7
+        itr_sigma = [0]*num_itr_plot
+        for itr in range(1, self.n_iter+1):
+            if itr > self.n_iter - num_itr_plot:
+                Sigma_iw_sh = self.solver.Sigma_iw_sh(itr)
 
-    #
-    # Read Sigma and average it
-    #
-    sigma_ave = []
-    nsigma = 0
-    num_itr_plot = 7
-    itr_sigma = [0]*num_itr_plot
-    for itr in range(1, iteration_number+1):
-        if itr > iteration_number - num_itr_plot:
-            Sigma_iw_sh = solver.Sigma_iw_sh(itr)
+                itr_sigma[nsigma] = itr
+                sigma_ave.append(GfImFreq(indices=[0], beta=self.beta, n_points=self.p["system"]["n_iw"]))
+                sigma_ave[nsigma].data[:, 0, 0] = 0.0
+                norb_tot = 0
+                for ish in range(self.n_sh):
+                    norb = self.shell_info[ish]['block_dim']
+                    for isp in self.spin_names:
+                        for iorb in range(norb):
+                            norb_tot += 1
+                            for jorb in range(norb):
+                                sigma_ave[nsigma].data[:, 0, 0] += Sigma_iw_sh[ish][isp].data[:, iorb, jorb]
+                sigma_ave[nsigma].data[:, 0, 0] /= norb_tot
+                nsigma += 1
 
-            itr_sigma[nsigma] = itr
-            sigma_ave.append(GfImFreq(indices=[0], beta=beta, n_points=p["system"]["n_iw"]))
-            sigma_ave[nsigma].data[:, 0, 0] = 0.0
-            norb_tot = 0
-            for ish in range(nsh):
-                norb = shell_info[ish]['block_dim']
-                for isp in spn:
-                    for iorb in range(norb):
-                        norb_tot += 1
-                        for jorb in range(norb):
-                            sigma_ave[nsigma].data[:, 0, 0] += Sigma_iw_sh[ish][isp].data[:, iorb, jorb]
-            sigma_ave[nsigma].data[:, 0, 0] /= norb_tot
-            nsigma += 1
-    #
-    # Real part
-    #
-    plt.subplot(gs[0])
-    for itr in range(nsigma):
-        oplot(sigma_ave[itr], '-o', mode='R', x_window=(0.0, omega_check), name='Sigma-%s' % itr_sigma[itr])
-    plt.legend(loc=0)
-    #
-    # Imaginary part
-    #
-    plt.subplot(gs[1])
-    for itr in range(nsigma):
-        oplot(sigma_ave[itr], '-o', mode='I', x_window=(0.0, omega_check), name='Sigma-%s' % itr_sigma[itr])
-    plt.legend(loc=0)
+        self.plt.figure(figsize=(8, 10))
+        gs = GridSpec(2, 1)
+        #
+        # Real part
+        #
+        self.plt.subplot(gs[0])
+        for itr in range(nsigma):
+            self.oplot(sigma_ave[itr], '-o', mode='R', x_window=(0.0, self.omega_check), name='Sigma-%s' % itr_sigma[itr])
+        self.plt.legend(loc=0)
+        #
+        # Imaginary part
+        #
+        self.plt.subplot(gs[1])
+        for itr in range(nsigma):
+            self.oplot(sigma_ave[itr], '-o', mode='I', x_window=(0.0, self.omega_check), name='Sigma-%s' % itr_sigma[itr])
+        self.plt.legend(loc=0)
 
-    plt.show()
-    if fileplot is not None:
-        plt.savefig(fileplot)
-    #
-    # Output Sigma into a text file
-    #
-    print("\n Output Local Self Energy : ", p["model"]["seedname"] + "_sigma.dat")
-    with open(p["model"]["seedname"] + "_sigma.dat", 'w') as fo:
-        print("# Local self energy at imaginary frequency", file=fo)
-        #
-        # Column information
-        #
-        print("# [Column] Data", file=fo)
-        print("# [1] Frequency", file=fo)
-        icol = 1
-        for ish in range(nsh):
-            norb = shell_info[ish]['block_dim']
-            for isp in spn:
-                for iorb in range(norb):
-                    for jorb in range(norb):
-                        icol += 1
-                        print("# [%d] Re(Sigma_{shell=%d, spin=%s, %d, %d})" % (icol, ish, isp, iorb, jorb), file=fo)
-                        icol += 1
-                        print("# [%d] Im(Sigma_{shell=%d, spin=%s, %d, %d})" % (icol, ish, isp, iorb, jorb), file=fo)
-        #
-        # Write data
-        #
-        Sigma_iw_tmp = solver.Sigma_iw_sh(iteration_number)
-        omega = [x for x in Sigma_iw_sh[0].mesh]
-        for iom in range(len(omega)):
-            print("%f " % omega[iom].imag, end="", file=fo)
-            for ish in range(nsh):
-                norb = shell_info[ish]['block_dim']
-                for isp, iorb, jorb in product(spn, range(norb), range(norb)):
-                    print("%f %f " % (Sigma_iw_tmp[ish][isp].data[iom, iorb, jorb].real,
-                                      Sigma_iw_tmp[ish][isp].data[iom, iorb, jorb].imag), end="", file=fo)
-            print("", file=fo)
-    #
-    # Output Legendre polynomial
-    #
-    """
-    if p["system"]["n_l"] > 0:
-        #
-        # Output Sigma into a text file
-        #
-        print("\n Output Local Self Energy : ", p["model"]["seedname"] + "_legendre.dat")
-        with open(p["model"]["seedname"] + "_legendre.dat", 'w') as fo:
+        filename = basename + fig_ext
+        self.plt.savefig(filename)
+        print(" Output " + filename)
+
+
+    def write_sigma_text(self, basename):
+        """
+        Output Sigma into a text file
+        """
+
+        filename = basename + ".dat"
+        with open(filename, 'w') as fo:
             print("# Local self energy at imaginary frequency", file=fo)
             #
             # Column information
             #
             print("# [Column] Data", file=fo)
-            print("# [1] Order of Legendre polynomials", file=fo)
+            print("# [1] Frequency", file=fo)
             icol = 1
-            for ish in range(nsh):
-                sol[ish].G_l << ar[output_group]['G_l'][str(ish)]
-                spn = solver.SK.spin_block_names[solver.SK.corr_shells[solver.SK.inequiv_to_corr[ish]]['SO']]
-                norb = solver.SK.corr_shells[solver.SK.inequiv_to_corr[ish]]['dim']
-                for isp in spn:
+            for ish in range(self.n_sh):
+                norb = self.shell_info[ish]['block_dim']
+                for isp in self.spin_names:
                     for iorb in range(norb):
                         for jorb in range(norb):
                             icol += 1
-                            print("# [%d] Re(G_l_{shell=%d, spin=%s, %d, %d})" % (icol, ish, isp, iorb, jorb),
-                                  file=fo)
+                            print("# [%d] Re(Sigma_{shell=%d, spin=%s, %d, %d})" % (icol, ish, isp, iorb, jorb), file=fo)
                             icol += 1
-                            print("# [%d] Im(G_l_{shell=%d, spin=%s, %d, %d})" % (icol, ish, isp, iorb, jorb),
-                                  file=fo)
+                            print("# [%d] Im(Sigma_{shell=%d, spin=%s, %d, %d})" % (icol, ish, isp, iorb, jorb), file=fo)
             #
             # Write data
             #
-            for il in range(p["system"]["n_l"]):
-                print("%d " % il, end="", file=fo)
-                for ish in range(nsh):
-                    spn = solver.SK.spin_block_names[solver.SK.corr_shells[solver.SK.inequiv_to_corr[ish]]['SO']]
-                    norb = solver.SK.corr_shells[solver.SK.inequiv_to_corr[ish]]['dim']
-                    for isp in spn:
-                        for iorb in range(norb):
-                            for jorb in range(norb):
-                                print("%f %f " % (sol[ish].G_l[isp].data[il, iorb, jorb].real,
-                                                  sol[ish].G_l[isp].data[il, iorb, jorb].imag), end="", file=fo)
+            Sigma_iw_tmp = self.solver.Sigma_iw_sh(self.n_iter)
+            # omega = [x for x in Sigma_iw_sh[0].mesh]
+            omega = [x for x in Sigma_iw_tmp[0].mesh]
+            for iom in range(len(omega)):
+                print("%f " % omega[iom].imag, end="", file=fo)
+                for ish in range(self.n_sh):
+                    norb = self.shell_info[ish]['block_dim']
+                    for isp, iorb, jorb in product(self.spin_names, range(norb), range(norb)):
+                        print("%f %f " % (Sigma_iw_tmp[ish][isp].data[iom, iorb, jorb].real,
+                                          Sigma_iw_tmp[ish][isp].data[iom, iorb, jorb].imag), end="", file=fo)
                 print("", file=fo)
-    """
 
-    #
-    # Finish
-    #
-    print("\n  Done\n")
+        print(" Output " + filename)
+
+
+    def plot_iter_mu(self, basename, fig_ext):
+        """
+        plot chemical potential as a function of iteration number
+        """
+        self.__plot_init()
+
+        iter = [ itr for itr in range(1, self.n_iter+1) ]
+        mu = [ self.solver.chemical_potential(itr) for itr in range(1, self.n_iter+1) ]
+
+        # save data
+        filename = basename + ".dat"
+        numpy.savetxt(filename, zip(iter, mu), fmt="%d %.10e")
+        print(" Output " + filename)
+
+        # plot
+        filename = basename + fig_ext
+        self.plt.figure(figsize=(8, 10))
+        gs = GridSpec(2, 1)
+
+        self.plt.subplot(gs[0])
+        self.plt.plot(iter, mu, marker="o")
+        self.plt.xlabel("iterations")
+        self.plt.ylabel("$\mu$")
+
+        # diff
+        self.plt.subplot(gs[1])
+        self.plt.plot(iter[1:], abs(numpy.array(mu[1:])-numpy.array(mu[:-1])), marker="o")
+        self.plt.xlabel("iterations")
+        self.plt.ylabel("diff")
+        self.plt.yscale("log")
+
+        self.plt.savefig(filename)
+        print(" Output " + filename)
+
+    def plot_iter_sigma(self, basename, fig_ext):
+        """
+        plot renormalization factor as a function of iteration number
+        """
+        self.__plot_init()
+
+        iter = [ itr for itr in range(1, self.n_iter+1) ]
+        label_all = []
+        z_all = []
+
+        w0 = self.n_iw
+        for ish in range(self.n_sh):
+            norb = self.shell_info[ish]['block_dim']
+            for isp, iorb, jorb in product(self.spin_names, range(norb), range(norb)):
+                sigma0 = numpy.array([ self.solver.Sigma_iw_sh(itr)[ish][isp].data[w0, iorb, jorb].imag
+                                       for itr in range(1, self.n_iter+1) ])
+                z = 1./(1-sigma0/numpy.pi*self.beta)
+                z_all.append(z)
+                label_all.append("shell=%d, spin=%s, %d, %d" %(ish,isp,iorb,jorb))
+
+        # save data
+        filename = basename + ".dat"
+        with open(filename, "w") as f:
+            for i, itr in enumerate(iter):
+                print(itr, file=f, end="")
+                for z in z_all:
+                    print("", z[i], file=f, end="")
+                print("", file=f)
+        print(" Output " + filename)
+
+        # plot
+        filename = basename + fig_ext
+        self.plt.figure(figsize=(8, 10))
+        gs = GridSpec(2, 1)
+
+        self.plt.subplot(gs[0])
+        for z, label in zip(z_all, label_all):
+            self.plt.plot(iter, z, label=label, marker="o")
+        self.plt.xlabel("iterations")
+        self.plt.ylabel("Renormalization factor")
+        self.plt.legend()
+
+        # diff
+        self.plt.subplot(gs[1])
+        for z, label in zip(z_all, label_all):
+            self.plt.plot(iter[1:], abs(numpy.array(z[1:])-numpy.array(z[:-1])), label=label, marker="o")
+        self.plt.xlabel("iterations")
+        self.plt.ylabel("diff")
+        self.plt.yscale("log")
+        self.plt.legend()
+
+        self.plt.savefig(filename)
+        print(" Output " + filename)
+
+
+def dcore_check(ini_file, prefix, fig_ext):
+
+    # add a dot to the extension, e.g., 'png' --> '.png'
+    ext = fig_ext if fig_ext[0] == '.' else '.' + fig_ext
+
+    # make directory
+    dir = os.path.dirname(prefix)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    check = DMFTCoreCheck(ini_file)
+    check.print_chemical_potential()
+    check.write_sigma_text(basename=prefix+"sigma")
+    check.plot_sigma_ave(basename=prefix+"sigma_ave", fig_ext=ext)
+    check.plot_iter_mu(basename=prefix+"iter_mu", fig_ext=ext)
+    check.plot_iter_sigma(basename=prefix+"iter_sigma", fig_ext=ext)
 
 
 if __name__ == '__main__':
@@ -230,15 +317,32 @@ if __name__ == '__main__':
                         type=str,
                         help="input file name."
                         )
-    parser.add_argument('--output',
+    parser.add_argument('--prefix',
                         action='store',
-                        default=None,
+                        default='check/',
                         type=str,
-                        help='output file name (extension pdf, eps, jpg, etc)'
+                        help='prefix for output files'
                         )
+    parser.add_argument('--ext',
+                        action='store',
+                        default='png',
+                        type=str,
+                        help='file extension of output figures (png, pdf, eps, jpg, etc)'
+                        )
+    # for backward compatibility
+    # parser.add_argument('--output',
+    #                     action='store',
+    #                     default=None,
+    #                     type=str,
+    #                     help='not used (retained for backward compatibility)'
+    #                     )
+
+    # if args.prefix is not None:
+    #     warn("--output option is not used")
 
     args = parser.parse_args()
-    if os.path.isfile(args.path_input_file) is False:
-        print("Input file is not exist.")
-        sys.exit(-1)
-    dcore_check(args.path_input_file, args.output)
+
+    dcore_check(args.path_input_file, args.prefix, args.ext)
+
+    # Finish
+    print("\n  Done\n")
