83d7a09b739ffc64a90d803aced23797fa3cc027
Nayuta Takemori <takemori@okayama-u.ac.jp>
2019-02-22 16:47:56 +0900

    fix: typo

--- python/impurity_solvers/__init__.py
+++ python/impurity_solvers/__init__.py
@@ -2,12 +2,12 @@ from .triqs_cthyb import TRIQSCTHYBSolver
 from .triqs_hubbard_I import TRIQSHubbardISolver
 from .alps_cthyb import ALPSCTHYBSolver
 from .null_solver import NullSolver
-from .alps_cthyb-seg import ALPSCTHYB-SEGSolver
+from .alps_cthyb_seg import ALPSCTHYBSEGSolver
 
 solver_classes = {
     'TRIQS/cthyb': TRIQSCTHYBSolver,
     'TRIQS/hubbard-I': TRIQSHubbardISolver,
     'ALPS/cthyb': ALPSCTHYBSolver,
     'null': NullSolver,
-    'ALPS/cthyb-seg': ALPSCTHYB-SEGSolver,
+    'ALPS/cthyb-seg': ALPSCTHYBSEGSolver,
 }
