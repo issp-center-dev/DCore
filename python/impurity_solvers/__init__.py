95a5c3d6fb5b1dbc2d98aef889185a41c12ff4b3
Nayuta Takemori <takemori@okayama-u.ac.jp>
2019-02-22 16:21:38 +0900

    fix: ctseg to cthyb_seg, add key in program_options.py

--- python/impurity_solvers/__init__.py
+++ python/impurity_solvers/__init__.py
@@ -2,12 +2,12 @@ from .triqs_cthyb import TRIQSCTHYBSolver
 from .triqs_hubbard_I import TRIQSHubbardISolver
 from .alps_cthyb import ALPSCTHYBSolver
 from .null_solver import NullSolver
-from .alps_ctseg import ALPSCTSEGSolver
+from .alps_cthyb-seg import ALPSCTHYB-SEGSolver
 
 solver_classes = {
     'TRIQS/cthyb': TRIQSCTHYBSolver,
     'TRIQS/hubbard-I': TRIQSHubbardISolver,
     'ALPS/cthyb': ALPSCTHYBSolver,
     'null': NullSolver,
-    'ALPS/ctseg': ALPSCTSEGSolver,
+    'ALPS/cthyb-seg': ALPSCTHYB-SEGSolver,
 }
