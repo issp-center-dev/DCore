2ce784fc59e87ed526b49ce7fab496f2ad833d4f
Junya Otsuki <j.otsuki@okayama-u.ac.jp>
2019-03-07 11:32:29 +0900

    manip_database.py works for single-band Hubbard model

--- python/manip_database.py
+++ python/manip_database.py
@@ -23,6 +23,28 @@ import os
 from pytriqs.archive.hdf_archive import HDFArchive
 
 
+def expand_block_diag_matrix(mat1, mat2):
+    """
+    make a large-size matrix combining 2 matrices as diag(mat1, mat2)
+    """
+    dim1_l = mat1.shape[0]
+    dim1_r = mat1.shape[1]
+    dim2_l = mat2.shape[0]
+    dim2_r = mat2.shape[1]
+
+    mat = numpy.zeros((dim1_l + dim2_l, dim1_r + dim2_r), dtype=complex)
+    mat[0: dim1_l, 0: dim1_r] = mat1[:, :]
+    mat[dim1_l: dim1_l + dim2_l, dim1_r: dim1_r + dim2_r] = mat2[:, :]
+    return mat
+
+
+def double_matrix(mat):
+    """
+    make a double-size block matrix, diag(mat, mat)
+    """
+    return expand_block_diag_matrix(mat, mat)
+
+
 def turn_on_spin_orbit(h5_file_in, h5_file_out):
     if not os.path.exists(h5_file_in):
         print("file '{}' not found".format(h5_file_in))
@@ -55,32 +77,39 @@ def turn_on_spin_orbit(h5_file_in, h5_file_out):
         print("{:11s}:".format(key), data[key].shape)
 
     data_new = {}
+
+    # n_orbitals
     data_new['n_orbitals'] = numpy.zeros((nk, 1), dtype=int)
     data_new['n_orbitals'][:, 0] = data['n_orbitals'][:, sp[0]] + data['n_orbitals'][:, sp[1]]
 
     max_n_orbitals = data_new['n_orbitals'].max()
     print("max_n_orbitals =", max_n_orbitals)
 
-    data_new['proj_mat'] = numpy.zeros((nk, 1, n_corr_shells, max_n_orbitals, max_n_orbitals), dtype=complex)
-    for k in range(nk):
-        offset = 0
-        for s in sp:
-            norb = data['n_orbitals'][k, s]
-            data_new['proj_mat'][k, 0, :, offset:offset+norb, offset:offset+norb]\
-                = data['proj_mat'][k, s, :, 0:norb, 0:norb]
-            offset += norb
+    max_corr_shell_dim = max([ corr_shell['dim'] for corr_shell in data['corr_shells'] ])
+    max_corr_shell_dim *= 2
+    print("max_corr_shell_dim =", max_corr_shell_dim)
 
+    # proj_mat
+    data_new['proj_mat'] = numpy.zeros((nk, 1, n_corr_shells, max_corr_shell_dim, max_n_orbitals), dtype=complex)
+    for k in range(nk):
+        for j in range(n_corr_shells):
+            data_new['proj_mat'][k, 0, j] \
+                = expand_block_diag_matrix(data['proj_mat'][k, sp[0], j], data['proj_mat'][k, sp[1], j])
+
+    # hopping
     data_new['hopping'] = numpy.zeros((nk, 1, max_n_orbitals, max_n_orbitals), dtype=complex)
     for k in range(nk):
-        offset = 0
-        for s in sp:
-            norb = data['n_orbitals'][k, s]
-            data_new['hopping'][k, 0, offset:offset+norb, offset:offset+norb]\
-                = data['hopping'][k, s, 0:norb, 0:norb]
-            offset += norb
+        data_new['hopping'][k, 0] \
+            = expand_block_diag_matrix(data['hopping'][k, sp[0]], data['hopping'][k, sp[1]])
 
     for key in keys:
         print("{:11s}:".format(key), data_new[key].shape)
+
+    # rot_mat
+    data_new['rot_mat'] = [double_matrix(rot) for rot in data['rot_mat']]
+
+    # T
+    data_new['T'] = [double_matrix(t) for t in data['T']]
 
     #
     # set flags
@@ -90,18 +119,40 @@ def turn_on_spin_orbit(h5_file_in, h5_file_out):
 
     # corr_shells
     corr_shells = data['corr_shells']
-    print(corr_shells)
+    # print(corr_shells)
     for crsh in corr_shells:
         assert crsh['SO'] == 0
         crsh['SO'] = 1
+        crsh['dim'] *= 2
     data_new['corr_shells'] = corr_shells
+    # print(corr_shells)
+
+    #
+    # print changes in database
+    #
+    def print_formated(obj):
+        if isinstance(obj, numpy.ndarray):
+            print("    ", obj.shape)
+        elif isinstance(obj, list):
+            for x in obj:
+                print_formated(x)
+        else:
+            print("    ", obj)
+
+    print("Summary of changes in the database")
+    for key, d in data_new.items():
+        print(" -", key)
+        print_formated(data[key])
+        print_formated(d)
 
     #
     # save data
     #
     if h5_file_in == h5_file_out:
+        # overwrite only updated components
         data_save = data_new
     else:
+        # write all components
         data.update(data_new)
         data_save = data
 
