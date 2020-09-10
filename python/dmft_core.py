90cea766be9d6460d68feed990827acd3a466743
Junya Otsuki <j.otsuki@okayama-u.ac.jp>
2020-09-10 12:28:40 +0900

    clean up

--- python/dmft_core.py
+++ python/dmft_core.py
@@ -225,23 +225,6 @@ def calc_dc(dc_type, u_mat, dens_mat, spin_block_names, use_spin_orbit):
     if dc_type == "HF_DFT" or dc_type == "HF_imp":
         if use_spin_orbit:
             dim_tot = dens_mat["ud"].shape[0]  # 2 * num_orb
-            # dc_imp_sh["ud"] = numpy.zeros((dim_tot, dim_tot), numpy.complex_)
-            # for s1, i1, s2, i2 in product(range(2), range(num_orb), range(2), range(num_orb)):
-            #     #
-            #     # Hartree
-            #     #
-            #     dc_imp_sh["ud"][i1 + s1 * num_orb, i2 + s1 * num_orb] += numpy.sum(
-            #         u_mat[i1, 0:num_orb, i2, 0:num_orb] * dens_mat["ud"][s2 * num_orb:s2 * num_orb + num_orb,
-            #                                               s2 * num_orb:s2 * num_orb + num_orb]
-            #     )
-            #     #
-            #     # Exchange
-            #     #
-            #     dc_imp_sh["ud"][i1 + s1 * num_orb, i2 + s2 * num_orb] += numpy.sum(
-            #         u_mat[i1, 0:num_orb, 0:num_orb, i2]
-            #         * dens_mat["ud"][s2 * num_orb:s2 * num_orb + num_orb, s1 * num_orb:s1 * num_orb + num_orb]
-            #     )
-            # TODO: Check if the following code is equivalent to the above.
             dc_imp_sh["ud"] = numpy.zeros((dim_tot, dim_tot), numpy.complex_)
             dc_imp_sh["ud"] += numpy.einsum("ijkl, jl->ik", u_mat, dens_mat["ud"], optimize=True)  # Hartree
             dc_imp_sh["ud"] -= numpy.einsum("ijkl, jk->il", u_mat, dens_mat["ud"], optimize=True)  # Fock
