60114d1e03474bdb59cb18319cc1c15745f9683d
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-02-09 06:06:58 +0000

    Add chain_hubbardI* to examples

---
+++ examples/chain_hubbardI_so/run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# ---------------------------------
+# check path and environment variables
+
+. ../tools/tools.sh
+
+# check command
+check_command dcore_pre
+check_command dcore
+check_command dcore_check
+check_command dcore_post
+
+# set NUM_PROC=1 if not defined
+set_num_proc
+
+# ---------------------------------
+# create and move into a directory
+
+rm -rf results
+mkdir results
+cd results || exit
+
+# ---------------------------------
+# DCore
+
+ini=../dmft.ini
+
+echo "running dcore_pre..."
+dcore_pre $ini || exit
+
+echo "running dcore..."
+dcore --np $NUM_PROC $ini || exit
+
+echo "running dcore_check..."
+dcore_check $ini || exit
+
+echo "running dcore_post..."
+dcore_post --np $NUM_PROC $ini || exit
