Fix basis rotation of G2

src/dcore/impurity_solvers/base.py
@@ -310,7 +310,7 @@ def _rotate_basis(rot, u_matrix, use_spin_orbit, Gfs, X_dict, chi_dict):
 
         # X_{1234}(iW, iw, iw') = < c_1^+(iw) c_2(iw+iW) c_4^+(iw'+iW) c_3(iw') >
         array = numpy.einsum("ijklxyz,im,jn,ko,lp -> mnopxyz", array,
-                    numpy.conj(rot_spin_full), rot_spin_full, rot_spin_full, numpy.conj(rot_spin_full))
+                    rot_spin_full, numpy.conj(rot_spin_full), numpy.conj(rot_spin_full), rot_spin_full)
 
         X_dict.clear()
         _set_to_dict(X_dict, array)
@@ -323,7 +323,7 @@ def _rotate_basis(rot, u_matrix, use_spin_orbit, Gfs, X_dict, chi_dict):
 
         # chi_{1234}(iW) = < c_1^+ c_2 c_4^+ c_3 >(iW)
         array = numpy.einsum("ijklx,im,jn,ko,lp -> mnopx", array,
-                    numpy.conj(rot_spin_full), rot_spin_full, rot_spin_full, numpy.conj(rot_spin_full))
+                    rot_spin_full, numpy.conj(rot_spin_full), numpy.conj(rot_spin_full), rot_spin_full)
 
         chi_dict.clear()
         _set_to_dict(chi_dict, array)
@@ -334,14 +334,12 @@ def _rotate_basis(rot, u_matrix, use_spin_orbit, Gfs, X_dict, chi_dict):
 
 
 def _set_from_dict(x_dict, x_array):
-    print("_set_from_dict")
     for key, val in x_dict.items():
         i, j, k, l = key
         x_array[i, j, k, l] = val  # val is np.array
 
 
 def _set_to_dict(x_dict, x_array):
-    print("_set_to_dict")
     n1, n2, n3, n4 = x_array.shape[:4]
     for i, j, k, l in product(range(n1), range(n2), range(n3), range(n4)):
         if not numpy.all(np.abs(x_array[i, j, k, l]) < 1e-8):  # if not zero matrix

src/dcore/impurity_solvers/jo_cthyb_seg.py
@@ -389,14 +389,14 @@ class JOCTHYBSEGSolver(SolverBase):
             # x0_lo[i, j, wf, wf']  (wb=0)
             # x0_{12,34}(0; wf, wf') = gf_{21}(iw) * gf_{34}(iw')
             # x0_{11,33}(0; wf, wf') = gf_{11}(iw) * gf_{33}(iw')  [Gf is diagonal]
-            x0_lo = np.zeros((2*self.n_orb, 2*self.n_orb, 2*num_wf, 2*num_wf), dtype=complex)
+            x0_lo = np.zeros((2*self.n_orb, 2*self.n_orb, 2*num_wf, 2*num_wf), dtype=numpy.complex128)
             for i1, i3 in product(range(2*self.n_orb), repeat=2):
                 x0_lo[i1, i3, :, :] = gf[:2*num_wf, None, i1] * gf[None, :2*num_wf, i3]
 
             # x0_tr[i, j, wb, wf]  (wf=wf')
             # x0_{12,34}(wb; wf) = - gf_{31}(wf) * gf_{24}(wf+wb)
             # x0_{12,12}(wb; wf) = - gf_{11}(wf) * gf_{22}(wf+wb)  [Gf is diagonal]
-            x0_tr = np.zeros((2*self.n_orb, 2*self.n_orb, num_wb, 2*num_wf), dtype=complex)
+            x0_tr = np.zeros((2*self.n_orb, 2*self.n_orb, num_wb, 2*num_wf), dtype=numpy.complex128)
             for i1, i2 in product(range(2*self.n_orb), repeat=2):
                 for wb in range(num_wb):
                     x0_tr[i1, i2, wb, :] = - gf[0:2*num_wf, i1] * gf[wb:wb+2*num_wf, i2]