7e7769ed00300652039a472c1e928b9cbf1c34e3
Junya Otsuki <j.otsuki@okayama-u.ac.jp>
2024-09-04 23:41:21 +0900

    Sort eigenvalues; Convert H to real type if possible

--- src/dcore/impurity_solvers/scipy_sparse_main.py
+++ src/dcore/impurity_solvers/scipy_sparse_main.py
@@ -22,6 +22,24 @@ def kron(*ops):
     return r
 
 
+def sort_eigenvalues(vals, vecs):
+    # Sort eigenvalues and eigenvectors in ascending order
+    idx = np.argsort(vals)
+    vals = vals[idx]
+    vecs = vecs[:, idx]
+    return vals, vecs
+
+
+def convert_to_real(array, name="array"):
+    # Check if H0 and U_ijkl are complex. If not, convert to real.
+    if np.any(np.iscomplex(array)):
+        print(f"{name} is complex.")
+        return array
+    else:
+        print(f"{name} is real. -> Convert to real.")
+        return array.real
+
+
 def main():
     parser = argparse.ArgumentParser(description='Process a string argument.')
     parser.add_argument('filename', type=str, help='Input file in json format')
@@ -41,8 +59,8 @@ def main():
     n_iw = params['n_iw']
     flag_spin_conserve = params['flag_spin_conserve']
 
-
     # Load H0
+    print("\nLoading H0 and U_ijkl")
     h0 = np.load(params['file_h0'])
     print(h0.shape)
     # assert h0.shape == (n_flavors, n_flavors)
@@ -53,8 +71,12 @@ def main():
     print(umat.shape)
     assert umat.shape == (2*n_sites, 2*n_sites, 2*n_sites, 2*n_sites)
 
+    # Check if H0 and U_ijkl are complex. If not, convert to real.
+    h0 = convert_to_real(h0, name="H0")
+    umat = convert_to_real(umat, name="U_ijkl")
+
     dim = 2 ** (2*n_sites)
-    print("dimension of Hilbert space:", dim)
+    print("\nDimension of Hilbert space:", dim)
 
     # building blocks for creation/anihilation operators
     local_ops = make_local_ops()
@@ -72,7 +94,7 @@ def main():
     for i in range(2*n_sites):
         n_I = i
         n_F = 2*n_sites - i - 1
-        print(n_I, n_F)
+        # print(n_I, n_F)
         args = [I] * n_I + [cdag] + [F] * n_F
         Cdag[i] = kron(*args)
         C[i] = Cdag[i].T  # real (omit conj)
@@ -96,6 +118,10 @@ def main():
     assert isinstance(hamil, sp.spmatrix)
     assert hamil.shape == (dim, dim)
 
+    print("\nHamiltonian:")
+    print(" | type =", type(hamil))
+    print(" | dtype =", hamil.dtype)
+
     # TODO: Use particle number conservation
 
     # Use full diagonalization if dim is small
@@ -109,11 +135,19 @@ def main():
     else:
         print("\nn_eigen < dim\n  -> Use Lanczos method")
         E, eigvecs = sp.linalg.eigsh(hamil, k=n_eigen, which='SA')
+        # E, eigvecs = sp.linalg.eigsh(hamil, k=n_eigen, which='LM')
         # E, eigvecs = sp.linalg.eigsh(hamil, k=n_eigen, which='SA', v0=np.ones(dim))
         # ‘SA’ : Smallest (algebraic) eigenvalues.
 
     assert E.shape == (n_eigen,)
     assert eigvecs.shape == (dim, n_eigen)
+
+    # TODO: Sort eigenvalues and eigenvectors
+    # The eigenvalues are not ordered in scipy.sparse.linalg.eigsh()
+    # for complex Hermitian matrices since eigs() is called internally.
+
+    # Sort eigenvalues and eigenvectors
+    E, eigvecs = sort_eigenvalues(E, eigvecs)
 
     print("\nEigenvalues:")
     print(E)
@@ -137,23 +171,28 @@ def main():
     header = f"dim = {dim}\nn_eigen = {n_eigen}\ni  E_i  Boltzmann_weight"
     np.savetxt("eigenvalues.dat", indexed_E, fmt='%d %.8e %.5e', header=header)
 
-    # Save eigenvectors
-    print("Eigenvectors:")
-    print(eigvecs)
+    # Check if eigenvalues are ordered
+    # assert np.all(np.diff(E) >= 0)
 
-    print("\nweights")
-    print(eigvecs * eigvecs.conj())
+    # TODO: Save eigenvectors
+    # print("Eigenvectors:")
+    # print(eigvecs)
 
-    print("\nOrthonormality of eigenvectors:")
+    # print("\nweights")
+    # print(eigvecs * eigvecs.conj())
+
+    # print("\nOrthonormality of eigenvectors:")
     overlap = eigvecs.conj().T @ eigvecs  # <m|n>
-    print("m n <m|n>")
-    for m, n in np.ndindex(n_eigen, n_eigen):
-        print(m, n, overlap[m, n])
+    # print("m n <m|n>")
+    # for m, n in np.ndindex(n_eigen, n_eigen):
+    #     print(m, n, overlap[m, n])
 
     ignore_orthonormality = True
 
-    # overlap matrix must be identity
+    # Check orhotonormality of eigenvectors
+    print("\nCheck orthonormality of eigenvectors")
     if not np.allclose(overlap, np.identity(n_eigen)):
+        print("  not orthonormal")
         sys.stdout.flush()
         print("Warning: Eigenvectors are not orthonormal!", file=sys.stderr)
         if ignore_orthonormality:
@@ -161,6 +200,8 @@ def main():
         else:
             print("  -> Exit. Set ignore_orthonormality=True to ignore this warning.", file=sys.stderr)
             sys.exit(1)
+    else:
+        print("  orthogonal")
 
     # Matsubara frequencies
     iws = 1j * (2 * np.arange(n_iw) + 1) * np.pi / beta
