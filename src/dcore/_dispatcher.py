630a0aef6885b455f8a4f191b81ed9e344d42611
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2025-08-29 11:01:49 +0900

    stop Pade anacont if dcorelib is older than 0.9.7

--- src/dcore/_dispatcher.py
+++ src/dcore/_dispatcher.py
@@ -18,13 +18,18 @@ if TRIQS_COMPAT:
     from dcorelib.triqs_compat import mpi
     from dcorelib.triqs_compat.dft_tools import SumkDFT, SumkDFTTools
     from dcorelib.triqs_compat.plot import mpl_interface
+
+    from dcorelib import __version__ as backend_version
+
+    backend_lib = "dcorelib"
+
 else:
     triqs_libs = ['triqs', 'triqs_dft_tools']
     for l in triqs_libs:
         if not importlib.util.find_spec(l):
             sys.exit("ERROR: TRIQS library is not found. Please install TRIQS library or set DCORE_TRIQS_COMPAT=1")
 
-    from triqs.version import version as triqs_version
+    from triqs.version import version as backend_version
 
     if not "2" < triqs_version[0] < "4":
         sys.exit("ERROR: TRIQS version {} is not supported. Please install TRIQS version 3.x".format(triqs_version))
@@ -43,4 +48,6 @@ else:
     else:
         from .backend import _triqs_mpi as mpi
 
-    print("INFO: TRIQS library {} is used".format(triqs_version))
+    backend_lib = "TRIQS"
+
+print(f"INFO: {backend_lib} {backend_version} is used")
